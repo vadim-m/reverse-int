806adad7c995b2935cd61e8f3ea45e60c1a37a4c
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2022-07-31 21:32:11 +0300

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const resultNum = String(Math.abs(n)).split("").reverse().join("");
+
+    return +resultNum;
+};
